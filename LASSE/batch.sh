6a5c81c80ae707873762ee277ffa0b3265a375d2
au555720 <au555720@uni.au.dk>
2024-05-03 11:45:12 +0200

    Batch script for running, and then retrieving jobs

---
+++ LASSE/batch.sh
@@ -0,0 +1,28 @@
+#Assumes yamls are named 1..4
+
+outfile='log'
+> "$outfile"
+
+path=tests
+
+sleep 1
+
+for i in {1..4}; do
+    python -u  CharmmGuiAuto.py -i ${path}/test${i}_noR.yaml  >>$outfile 2>&1 &
+done
+
+obids_outfile='jobids.txt'
+> "$jobids_outfile"
+
+
+while [ $(cat $jobids_outfile | wc -l) -lt 4 ]; do
+    grep JOBID $outfile | awk '{print $NF}' > $jobids_outfile
+done
+
+for i in $(cat $jobids_outfile); do
+    sed "s/JOBID/$i/g"  ${path}/Retrieve.yaml > ${path}/${i}_retrieve.yaml
+    python -u  CharmmGuiAuto.py -i ${path}/${i}_retrieve.yaml  >>log_retrieve 2>&1 &
+done
+
+
+
